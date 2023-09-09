e1df6b7ef88c05412a87b51c969f89a21a96f2ca
WaldyWhite <w.weis@hotmail.com>
2023-09-09 13:14:58 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Vector vector = new Vector(5, 6, 8);
+        System.out.println(vector.vectorLength());
+        System.out.println(vector.scalarProduct(vector));
+
+    }
+}
---
+++ src/Vector.java
@@ -0,0 +1,24 @@
+public class Vector {
+    private double x;
+    private double y;
+    private double z;
+
+    // Constructor
+    public Vector (double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        }
+    // method that calculates the length of a vector Корень можно посчитать с помощью Math.sqrt()
+    public double vectorLength () {
+        return Math.sqrt(x * x + y *y + z * z);
+    }
+    // method that calculates the scalar product
+    public double scalarProduct(Vector vector) {
+        double result = x * vector.x + y * vector.y + z * vector.z;
+        return  result;
+
+    }
+    public double
+
+}
